943aeb041101d570e8baac64564c1644682bf3f7
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-12-21 17:00:59 -0300

    abstract class and basic classes created

---
+++ AppCadastro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+namespace AppCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
